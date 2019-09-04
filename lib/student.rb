da4fc746b4c240a47d8cbcf029e7279b65ca8627
github username <github email address>
2019-09-04 16:59:40 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -31,10 +31,11 @@ class Student
 
     def save
       sql = <<-SQL
-      INSERT INTO students (id, name, grade) 
-      VALUES (?, ?,?)
+      INSERT INTO students (name, grade) 
+      VALUES (?,?)
       SQL
-      DB[:conn].execute(sql, self.id, self.name, self.grade)
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
       #binding.pry
     end
 
